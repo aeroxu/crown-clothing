6cce15200a01620a8ef147fadb4337738692dde7
Xu <50079470+aeroxu@users.noreply.github.com>
2020-08-14 11:07:33 -0400

    updating our app with a shop page, collection preview component and collection item component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./homepage.styles.scss";
-import Directory from "../../comonents/directory/directory.component";
+import Directory from "../../components/directory/directory.component";
+
 
 const Homepage = ({ history }) => {
     return(
